1cb20ea86f90a56bb6a7557a1b0c2c6f766a48ba
Matheus Dias <saidaluap1@gmail.com>
2025-10-08 12:52:15 -0300

    Atualiza supabaseClient.js para usar variáveis de ambiente de produção

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
 const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = 'https://[seu-project-url].supabase.co'; // copie o seu
-const supabaseKey = '[sua-api-key]'; // copie a sua anon/public key do dashboard
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY; // ou ANON_KEY se preferir esse nome
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
